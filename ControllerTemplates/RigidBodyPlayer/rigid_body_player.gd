87badb92670dcfe8ef91c9b2aa87da39b68b5e95
Michael Eden <70781525+Deltt@users.noreply.github.com>
2025-09-08 22:21:08 +0200

    -

--- ControllerTemplates/RigidBodyPlayer/rigid_body_player.gd
+++ ControllerTemplates/RigidBodyPlayer/rigid_body_player.gd
@@ -1,8 +1,9 @@
-extends CharacterBody3D
+extends RigidBody3D
 
-class_name CharacterBodyPlayer
+class_name RigidBodyPlayer
 
 @export var network_position : Vector3
+@export var shape_cast : ShapeCast3D
 @export var synchronizer : MultiplayerSynchronizer
 
 const DEFAULT_SPEED = 5.0
@@ -10,6 +11,7 @@ const DEFAULT_JUMP_VELOCITY = 4.5
 
 func _ready() -> void:
 	network_position = position
+	pass
 
 func _physics_process(delta: float) -> void:
 	if !is_multiplayer_authority():
@@ -19,18 +21,17 @@ func _physics_process(delta: float) -> void:
 			position = network_position
 		return
 
-	if is_on_floor():
-		velocity.y = 0
-	velocity += get_gravity() * delta
+	#if is_on_floor():
+		#velocity.y = 0
+	#velocity += get_gravity() * delta
 
-	if Input.is_key_pressed(KEY_SPACE) and is_on_floor():
-		velocity.y = DEFAULT_JUMP_VELOCITY
+	if Input.is_key_pressed(KEY_SPACE) and shape_cast.is_colliding():
+		#apply_central_impulse(Vector3(0.0, 1.0, 0.0) * DEFAULT_JUMP_VELOCITY)
+		linear_velocity.y = DEFAULT_JUMP_VELOCITY
 
 	if Input.is_key_pressed(KEY_V):
-		velocity.y = DEFAULT_JUMP_VELOCITY
-
-	if Input.is_key_pressed(KEY_C):
-		velocity.y = 0.0
+		#apply_central_impulse(Vector3(0.0, 1.0, 0.0) * DEFAULT_JUMP_VELOCITY)
+		linear_velocity.y = DEFAULT_JUMP_VELOCITY
 
 	var key_input : Vector3
 	key_input.x = float(Input.is_key_pressed(KEY_D)) - float(Input.is_key_pressed(KEY_A))
@@ -39,9 +40,10 @@ func _physics_process(delta: float) -> void:
 	key_input.y = 0
 	key_input = key_input.normalized()
 	var movement : Vector3 = key_input * DEFAULT_SPEED
-	velocity.x = movement.x
-	velocity.z = movement.z
-	move_and_slide()
+	var current_xz_velocity : Vector3 = Vector3(linear_velocity.x, 0.0, linear_velocity.z)
+	if current_xz_velocity.length() <= DEFAULT_SPEED:
+		linear_velocity.x = movement.x
+		linear_velocity.z = movement.z
 
 	network_position = position
 	pass
